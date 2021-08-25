6e6c2a4e9cdeb18d1d9b7c2f384d254871bebe53
994710097 <994710097@qq.com>
2021-08-25 17:15:44 +0800

    css

--- .vuepress/components/Demo-1.vue
+++ .vuepress/components/Demo-1.vue
@@ -1,20 +1,21 @@
 <template>
   <div style="width: 200px; height: 200px">
-    <css-doodle @click="refresh" ref="doodle">
+    <div>123</div>
+    <!-- <css-doodle @click="refresh" ref="doodle">
       @grid: 50x1 / 100%; @place-cell: center; @size: calc(75% / @I * @i);
       transform: rotate(calc(@i * 5deg)); border-radius: 30%; border: 1px solid
       hsla( calc(10 + 4 * @i), 70%, 68%, @r.8 );
-    </css-doodle>
+    </css-doodle> -->
   </div>
 </template>
 
 <script>
-import "css-doodle";
+// import "css-doodle";
 export default {
-  methods: {
-    refresh: function () {
-      this.$refs.doodle.update();
-    },
-  },
+  // methods: {
+  //   refresh: function () {
+  //     this.$refs.doodle.update();
+  //   },
+  // },
 };
 </script>
