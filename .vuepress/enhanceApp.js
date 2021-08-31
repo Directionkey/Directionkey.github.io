0cd31ce98784f1c34a78800001d76e724e853e75
994710097 <994710097@qq.com>
2021-08-31 16:08:26 +0800

    vextab

--- .vuepress/enhanceApp.js
+++ .vuepress/enhanceApp.js
@@ -1,12 +1,6 @@
 export default ({ Vue, options, router, siteData }) => {
   Vue.use(Element);
   Vue.mixin({
-    mounted() {
-      import('vextab').then(function(m) {
-        Vue.component('VexTab', m.VexTab);
-        Vue.component('Artist', m.Artist);
-        Vue.component('Vex', m.Vex);
-      });
-    },
+    mounted() {},
   });
 };
