14bfe4b84e649e5b3cb1cec99d25fe55e3e1e3f0
994710097 <994710097@qq.com>
2021-08-31 15:30:11 +0800

    vextab

--- .vuepress/components/vexflow.vue
+++ .vuepress/components/vexflow.vue
@@ -1,16 +1,6 @@
 <template>
   <div>
     <div id="boo"></div>
-    <div class="vextab-auto" width=680 scale=1.0 editor="true" editor-width=680 editor-height=330>
-      options space=20
-      tabstave notation=true key=A time=4/4
-
-      notes :q =|: (5/2.5/3.7/4) :8 7-5h6/3 ^3^ 5h6-7/5 ^3^ :q 7V/4 |
-      notes :8 t12p7/4 s5s3/4 :8 3s:16:5-7/5 :h p5/4
-      text :w, |#segno, ,|, :hd, , #tr
-
-      options space=25
-    </div>
   </div>
 </template>
 
@@ -20,17 +10,11 @@ import { VexTab, Artist, Vex } from "vextab";
 export default {
   mounted() {
     this.rua();
+    console.log(this.data);
   },
+  props: ["data"],
   methods: {
     rua() {
-      const data = `
-        tabstave notation=true key=A time=4/4
-
-        notes :q =|: (5/2.5/3.7/4) :8 7-5h6/3 ^3^ 5h6-7/5 ^3^ :q 7V/4 |
-        notes :8 t12p7/4 s5s3/4 :8 3s:16:5-7/5 :q p5/4
-        text :w, |#segno, ,|, :hd, , #tr
-      `;
-
       const Renderer = Vex.Flow.Renderer;
 
       // Create VexFlow Renderer from canvas element with id #boo
@@ -41,7 +25,7 @@ export default {
       const tab = new VexTab(artist);
 
       try {
-        tab.parse(data);
+        tab.parse(this.data);
         artist.render(renderer);
       } catch (e) {
         console.error(e);
