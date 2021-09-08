441ffc7051910517307c012cf674a4c34e24aadc
994710097 <994710097@qq.com>
2021-09-08 15:47:04 +0800

    fix

---
+++ .vuepress/enhanceApp.js
@@ -0,0 +1,9 @@
+// import 'css-doodle';
+export default ({ Vue, options, router, siteData }) => {
+  // Vue.use()
+  // Vue.mixin({
+  //   mounted() {
+  //     import('css-doodle').then(function(module) {});
+  //   },
+  // });
+};
